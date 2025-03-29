6e7cd51136532803c75035d35ba526543fb62d7b
MirazZim <miraz.zim.38@gmail.com>
2025-03-29 12:17:34 +0600

    fix: add error handling for different environments (development/production) - Implemented custom error handler for rendering HTML or JSON responses based on environment. - Added error page rendering with details in development mode and a simplified message for production.

--- middlewares/Common/errorHandler.js
+++ middlewares/Common/errorHandler.js
@@ -2,14 +2,20 @@ const createError = require("http-errors");
 
 //404 error handler
 function notFoundHandler(req, res, next) {
- next(createError(404, "your requested content Not Found"));
+ next(createError(404, "Your requested content was not found"));
 }
 
 //default error handler
 function errorHandler(err, req, res, next) {
-  res.render('error',{
-    title: 'Error Page'
-  });
+  res.locals.error = process.env.NODE_ENV === 'development' ? err : {message: err.message};
+  res.status(err.status || 500);
+  if (res.locals.html) {
+    //html
+    res.render('error', {title: 'Error Page'});
+  } else {
+    //json
+    res.json(res.locals.error);
+  }
 }
 
 module.exports = {notFoundHandler, errorHandler};
